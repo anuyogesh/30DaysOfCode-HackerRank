89d01299aa1fb5028cdf3755576602a05e9df228
Anu Yogesh <69185499+anuyogesh@users.noreply.github.com>
2020-09-08 22:55:17 +0530

    Exceptions - Strings to Integers

---
+++ day16.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+     public static void main(String[] args) {
+        try {
+            System.out.println(Integer.valueOf(new Scanner(System.in).next()));
+        } catch (Exception e) {
+            System.out.println("Bad String");
+        }
+    }
+}
